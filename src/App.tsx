c55c61ebea6d666cbee536d0d52076e36c0302b4
JAMES WU <wujames989@gmail.com>
2026-02-10 20:44:24 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript James Wu
+                UD CISC275 with React Hooks and TypeScript 
+                James Wu
+		Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
